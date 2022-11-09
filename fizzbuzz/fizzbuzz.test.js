6733a0398aa7003b163d6188903c7430827711af
Abbey <abbey@Abbeys-MBP.lan>
2022-11-09 23:35:08 +0000

    Create first test

---
+++ fizzbuzz/fizzbuzz.test.js
@@ -0,0 +1,9 @@
+// const {add, subtract} = require('./maths');
+
+test('Array contains 1 to 100', () => {
+    expect(populateArray()).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]);
+});
+
+// test('subtracts two', () => {
+//     expect(subtract(9, 8)).toBe(1);
+// });
