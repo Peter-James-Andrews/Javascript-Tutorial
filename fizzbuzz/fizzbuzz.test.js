8bf846f4cfd923aa92579ba87c82ceab92e145df
Abbey <abbey@Abbeys-MBP.lan>
2022-11-09 23:49:14 +0000

    Create array 1-15

--- fizzbuzz/fizzbuzz.test.js
+++ fizzbuzz/fizzbuzz.test.js
@@ -1,6 +1,6 @@
-// const {add, subtract} = require('./maths');
+const {populateArray} = require('./fizzbuzz');
 
-test('Array contains 1 to 100', () => {
+test('Array contains 1 to 15', () => {
     expect(populateArray()).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]);
 });
 
