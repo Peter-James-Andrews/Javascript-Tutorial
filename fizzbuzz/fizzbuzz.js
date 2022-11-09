42792462c19ac2700834a1410ed3ea6003eabe56
Abbey <abbey@Abbeys-MBP.lan>
2022-11-09 23:58:46 +0000

    Refactor array number generator

--- fizzbuzz/fizzbuzz.js
+++ fizzbuzz/fizzbuzz.js
@@ -1,5 +1,9 @@
 module.exports = {populateArray};
 
-function populateArray() {
-   return [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+function populateArray(length) {
+    var numbers = []
+    for (let i = 1; i < length; i++) {
+        numbers.push(i);
+    }
+    return numbers
 };
--- fizzbuzz/fizzbuzz.test.js
+++ fizzbuzz/fizzbuzz.test.js
@@ -1,7 +1,7 @@
 const {populateArray} = require('./fizzbuzz');
 
 test('Array contains 1 to 15', () => {
-    expect(populateArray()).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]);
+    expect(populateArray(16)).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]);
 });
 
 // test('subtracts two', () => {
